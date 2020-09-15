15f8486dd02811775a9e7a9f889d5451e1c1e1b0
Tobiny <fernandochajim@gmail.com>
2020-09-15 17:39:09 -0500

    Metodo de impresión
    Imprimir tablero ahora es un método

--- Gato.java
+++ Gato.java
@@ -1,3 +1,4 @@
+
 public class Gato{
     public static void main(String[] args) {
         char[][] tableroJuego = {{' ', '|', ' ', '|', ' '},
@@ -5,8 +6,10 @@ public class Gato{
                                  {' ', '|', ' ', '|', ' '},
                                  {'-', '+', '-', '+', '-'},
                                  {' ', '|', ' ', '|', ' '}};
+    }
+    public static void imprimirTablero(){
         for (char[] fila : tableroJuego) {
-            for (char[] i : fila) {
+            for (char i : fila) {
                 System.out.print(i);
             }
             System.out.println();
