c8f7d4ec9b72bba66d962439e57acead767e78af
Tobiny <fernandochajim@gmail.com>
2020-09-16 13:52:32 -0500

    Cpu y usuario
    La computadora ahora tiene juego también

--- Gato.java
+++ Gato.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Gato{
     public static void main(String[] args) {
         char[][] tableroJuego = {{' ', '|', ' ', '|', ' '},
@@ -6,15 +7,23 @@ public class Gato{
                                  {' ', '|', ' ', '|', ' '},
                                  {'-', '+', '-', '+', '-'},
                                  {' ', '|', ' ', '|', ' '}};
+
         imprimirTablero(tableroJuego);
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese la posición a seleccionar (1 al 9): ");
-        int pos = sc.nextInt();
+        int posJugador = sc.nextInt();
 
-        
+        asignarPieza(tableroJuego, posJugador, "jugador");
+
+        Random aleatorio = new Random();
+        int posCpu = aleatorio.nextInt(9) + 1;
+        asignarPieza(tableroJuego, posCpu, "cpu");
+
+        imprimirTablero(tableroJuego);
 
     }
+    //Método para imprimir el tablero de juego
     public static void imprimirTablero(char[][] tableroJuego){
         for (char[] fila : tableroJuego) {
             for (char i : fila) {
@@ -23,10 +32,10 @@ public class Gato{
             System.out.println();
         }
     }
-
+    //Método para asignar la pieza a la posición correspondiente
     public static void asignarPieza(char[][] tableroJuego, int pos, String usuario){
         
-        char simbolo = 'X';
+        char simbolo = ' ';
         if(usuario.equals("jugador"))
             simbolo = 'X';
         else if(usuario.equals("cpu"))
